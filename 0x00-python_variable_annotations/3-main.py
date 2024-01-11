3537b08e71ab2d588ab4577051bcee237278df2d
codeabuu <abdulngui718@gmail.com>
2024-01-11 21:13:04 +0000

    return string form float n

---
+++ 0x00-python_variable_annotations/3-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+to_str = __import__('3-to_str').to_str
+
+pi_str = to_str(3.14)
+print(pi_str == str(3.14))
+print(to_str.__annotations__)
+print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''reutrn string from float'''
+
+
+def to_str(n: float) -> str:
+    '''return'''
+    return str(n)
