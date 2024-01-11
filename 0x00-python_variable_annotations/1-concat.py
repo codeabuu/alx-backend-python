a2e3334c36b724cd3bf1c59edba57504e61631e7
codeabuu <abdulngui718@gmail.com>
2024-01-11 20:59:58 +0000

    conc strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''return conc strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''return expected'''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+concat = __import__('1-concat').concat
+
+str1 = "egg"
+str2 = "shell"
+
+print(concat(str1, str2) == "{}{}".format(str1, str2))
+print(concat.__annotations__)
