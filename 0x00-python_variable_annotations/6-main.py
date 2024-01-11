8cc1262465c07efd4281819cd98bad5cc7b02c0d
codeabuu <abdulngui718@gmail.com>
2024-01-11 21:55:09 +0000

    using union instance to conc to var types

---
+++ 0x00-python_variable_annotations/6-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
+
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
+
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''takes a list mxd_lst of integers and
+floats and returns their sum as a float.'''
+
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''return'''
+    return float(sum(mxd_lst))
+
