4b52657c46eaa086966e2fbb73d7efcdce519d48
codeabuu <abdulngui718@gmail.com>
2024-01-15 20:09:36 +0000

    updates

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments'''
+
+
+import asyncio
+wait_random = __import__ ('0-basic_async_syntax').wait_random
+import typing
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    '''loop and return'''
+    delays = []
+    p = []
+
+    for i in range(n):
+        tasks = wait_random(max_delay)
+        p.append(tasks)
+
+    for tasks in asyncio.as_completed((p)):
+        delay = await task
+        delays.append(delay)
+
+    return delays
