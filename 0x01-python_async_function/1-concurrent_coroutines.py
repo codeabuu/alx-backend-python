c145bb15c1732b777050066fad4f576b1d4ca74b
codeabuu <abdulngui718@gmail.com>
2024-01-15 20:13:09 +0000

    mport wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-'''Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments'''
+'''Import wait_random from the previous python file
+that you’ve written and write an async routine
+called wait_n that takes in 2 int arguments'''
 
 
-import asyncio
-wait_random = __import__ ('0-basic_async_syntax').wait_random
 import typing
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
