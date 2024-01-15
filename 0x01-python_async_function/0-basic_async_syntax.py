fc58223518f632a299cd809f1fdfccccad3f0c15
codeabuu <abdulngui718@gmail.com>
2024-01-15 11:22:44 +0000

    aysnc and await to return a delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     '''function to await'''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
