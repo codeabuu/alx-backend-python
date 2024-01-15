72d3cecc2f925218fefb49ab6e082b4cac6a3d20
codeabuu <abdulngui718@gmail.com>
2024-01-15 20:16:38 +0000

    mport wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''loop and return'''
     delays = []
     p = []
